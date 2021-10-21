ff495d29d65f258d8e7c5f2908d1587d953d6254
DubiousPhoenix <nihilisticphoenix@gmail.com>
2021-10-21 11:08:29 -0400

    Add files via upload

---
+++ Win-10-Bulk-Installer.bat
@@ -0,0 +1,61 @@
+echo OFF
+
+NET SESSION >nul 2>&1
+
+IF %ERRORLEVEL% EQU 0 (
+
+   echo.
+
+) ELSE (
+
+   echo.-------------------------------------------------------------
+   echo                  Error: I need admin permissions.
+   echo                    So run me as an admin 4head
+   echo. -------------------------------------------------------------
+   pause
+   EXIT /B 1
+
+)
+
+powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco feature enable -n=allowGlobalConfirmation
+
+echo Ok, ready to go!
+pause
+
+@rem ----[ Program List ]----
+choco install 7zip.portable
+choco install audacity
+choco install audacity-ffmpeg
+choco install audacity-lame
+choco install bleachbit.portable
+choco install calibre
+choco install discord-canary
+choco install dropbox
+choco install epicgameslauncher
+choco install lghub
+choco install goggalaxy
+choco install jdownloader
+choco install keypirinha
+choco install krita
+choco install mpv.portable
+choco install qbittorrent
+choco install radmin-vpn
+choco install rainmeter
+choco install resilio-sync-home
+choco install sharex
+choco install nuclear
+choco install steam-client
+choco install streamlabs-obs
+choco install vivaldi.portable
+choco install vortex
+choco install vscode
+choco install vscode-prettier
+choco install notion
+@rem ----[ Program List ]----
+pause
+
+echo.
+echo All programs installed. Welcome Back to Windows o/
+pause
